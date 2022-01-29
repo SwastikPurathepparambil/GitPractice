4ec38f644b453e7bb4fb7bbb025d2c7118de2093
Swastik Purathepparambil <swastiksp25@gmail.com>
2022-01-29 12:03:51 -0800

    VSCode actual code

--- GitPractice/VSCodeEdit.js
+++ GitPractice/VSCodeEdit.js
@@ -1,7 +1,5 @@
 const array = ["Cat", "Dog", "Dog", "Emu", "Tiger"];
 
-// checks whether an element is even
 const dogs = (element) => element === "Dog";
 
 console.log(array.some(dogs));
-// expected output: true
